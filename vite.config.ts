9dcf77435a542d4ba14a906bdeae0659534233d0
unknown <kkan0615@gmail.com>
2022-06-06 14:14:25 -0400

    :zap: Feat: add vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig, } from 'vite';
+import { defineConfig, } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   server: {
     open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
